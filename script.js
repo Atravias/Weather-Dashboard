ff5540e704a22715d1ff7f68ce75b8d34f437a13
Atravias Borley <atravias@gmail.com>
2020-01-05 18:44:23 -0700

    got the first api working!

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var city = "Murray";
-// var search = $(".search");
-var state = $(".state").text;
+var city = $('.city');
 var APIKey = "166a433c57516f51dfab1f7edaed8413";
 var queryUrl = "https://api.openweathermap.org/data/2.5/weather?" +
     "q=" + city + ",utah&units=imperial&appid=" + APIKey;
@@ -9,8 +7,10 @@ var queryUrl = "https://api.openweathermap.org/data/2.5/weather?" +
 var submit = $('.sbmt');
 
 $(submit).on("click", function () {
-    var myJson = JSON.stringify(city)
+    var city = $(".city").val().trim()
 
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",utah&units=imperial&appid=" + APIKey;
     $.ajax({
         url: queryUrl,
         method: "GET"
@@ -18,7 +18,9 @@ $(submit).on("click", function () {
         // We store all of the retrieved data inside of an object called "response"
         .then(function (response) {
 
-            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            console.log(response)
+
+            $('.city').html("<h2>" + response.name + "</h2>");
             $(".wind").text("Wind Speed: " + response.wind.speed);
             $(".humidity").text("Humidity: " + response.main.humidity);
             $(".temp").text("Temperature (F) " + response.main.temp);
